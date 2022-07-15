620843aa0a4e81ee1c46715b41e77ceb6e55f89b
Felipe Flores <70114490+floresfelipesc@users.noreply.github.com>
2022-07-15 17:32:23 -0400

    BinaryTree Implementation

---
+++ ConsoleApp1/BinaryNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTesting
+{
+    public class BinaryNode
+    {
+        public int data;
+        public BinaryNode leftNode;
+        public BinaryNode rightNode;
+
+        public BinaryNode()
+        {
+            data = -1;
+            leftNode = null;
+            rightNode = null;
+        }
+
+        public BinaryNode(int data)
+        {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+    }
+}
---
+++ ConsoleApp1/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTesting
+{
+    public class BinaryTree
+    {
+        public BinaryNode root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void Insertion(int data)
+        {
+            BinaryNode tempNode = new BinaryNode(data);
+
+            if(root == null)
+            {
+                root = tempNode;
+                return;
+            }
+
+            tempNode = root;
+            BinaryNode previous = new BinaryNode();
+
+            while(tempNode != null)
+            {
+                previous = tempNode;
+                if(data <= tempNode.data)
+                {
+                    tempNode = tempNode.leftNode;
+                }
+                else
+                {
+                    tempNode = tempNode.rightNode;
+                }
+                
+            }
+
+            if(data <= previous.data)
+            {
+                previous.leftNode = new BinaryNode(data);
+            }
+            else
+            {
+                previous.rightNode = new BinaryNode(data);
+            }
+            
+                
+           
+
+        }
+    }
+}
