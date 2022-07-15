2c806823dac6c808ef314fbe2d9c3b3a3e99ee84
Felipe Flores <70114490+floresfelipesc@users.noreply.github.com>
2022-07-15 17:32:29 -0400

    Create BinaryTreeTests.cs

---
+++ DataStructuresTestingUnitTests/BinaryTreeTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructuresTesting;
+
+namespace DataStructuresTestingUnitTests
+{
+    [TestClass]
+    public class BinaryTreeTests
+    {
+        [TestMethod]
+        public void Insertion_TreeEmpty_ReturnSuccess(){
+            //initialize
+            BinaryTree myTree = new BinaryTree();
+            int expected = 1;
+            int actual = -1;
+            int input = 1;
+            //act
+            myTree.Insertion(input);
+            actual = myTree.root.data;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Insertion_TreeOccupied_ReturnSuccess()
+        {
+            //initialize
+            BinaryTree myTree = new BinaryTree();
+            int expected = 2;
+            int actual;
+
+            //act
+            myTree.Insertion(1);
+            myTree.Insertion(2);
+            actual = myTree.root.rightNode.data;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
