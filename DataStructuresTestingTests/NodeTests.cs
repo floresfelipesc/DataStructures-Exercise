61a6f3714675ca93e96c2ec35bd71f7aace0265a
Felipe Flores <70114490+floresfelipesc@users.noreply.github.com>
2024-04-17 16:51:21 -0400

    Node Tests

---
+++ DataStructuresTestingTests/NodeTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructuresTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresTesting.Tests
+{
+    [TestClass()]
+    public class NodeTests
+    {
+        [TestMethod()]
+        public void NodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
